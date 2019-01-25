ed8ce24b3b51502fd7a66383b57304f775b3d1c3
Jimyung6642 <cory66421@gmail.com>
2019-01-25 11:51:15 +0900

    Use temp mapping table and use only 5 characters of div_cd for mapping

--- inst/sql/sql server/110.Procedure_occurrence.sql
+++ inst/sql/sql server/110.Procedure_occurrence.sql
@@ -7,9 +7,9 @@
  @NHISNSC_database : DB for NHIS-NSC in CDM format
  @NHIS_JK: JK table in NHIS NSC
  @NHIS_20T: 20 table in NHIS NSC
- @NHIS_30T: 30 table in NHIS NSC
+ @@NHIS_30T: 30 table in NHIS NSC
  @NHIS_40T: 40 table in NHIS NSC
- @NHIS_60T: 60 table in NHIS NSC
+ @@NHIS_60T: 60 table in NHIS NSC
  @NHIS_GJ: GJ table in NHIS NSC
  @CONDITION_MAPPINGTABLE : mapping table between KCD and OMOP vocabulary
  @DRUG_MAPPINGTABLE : mapping table between EDI and OMOP vocabulary
@@ -23,22 +23,23 @@
 /**************************************
  1. 변환 데이터 건수 파악
 ***************************************/ 
+/*
 -- 30T 변환 예상 건수(1:N 매핑 허용)
 select count(a.key_seq)
-from @NHISNSC_rawdata.@NHIS_30T a, (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) b, @NHISNSC_rawdata.@NHIS_20T c
+from @NHISNSC_rawdata.@@NHIS_30T a, (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) b, @NHISNSC_rawdata.@NHIS_20T c
 where a.div_cd=b.source_code
 and a.key_seq=c.key_seq
 
 -- 참고) 30T 변환 예상 건수 (distinct 용어만 카운트)
 select count(a.key_seq)
-from @NHISNSC_rawdata.@NHIS_30T a, @NHISNSC_rawdata.@NHIS_20T b
+from @NHISNSC_rawdata.@@NHIS_30T a, @NHISNSC_rawdata.@NHIS_20T b
 where a.key_seq=b.key_seq
 and a.div_cd in (select distinct c.source_code
 	from (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) as c)
 	
 -- 참고) 30T 중 1:N 매핑 중복 건수
 select count(a.key_seq), sum(cnt)
-from @NHISNSC_rawdata.@NHIS_30T a, 
+from @NHISNSC_rawdata.@@NHIS_30T a, 
 	(select source_code, count(source_code)-1 as cnt 
 	from (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) as c
 	group by source_code 
@@ -49,20 +50,20 @@ where a.div_cd=b.source_code
 ----------------------------------------
 -- 60T 변환 예상 건수(1:N 매핑 허용)
 select count(a.key_seq)
-from @NHISNSC_rawdata.@NHIS_60T a, (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) b, @NHISNSC_rawdata.@NHIS_20T c
+from @NHISNSC_rawdata.@@NHIS_60T a, (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) b, @NHISNSC_rawdata.@NHIS_20T c
 where a.div_cd=b.source_code
 and a.key_seq=c.key_seq
 
 -- 참고) 60T 변환 예상 건수 (distinct 용어만 카운트)
 select count(a.key_seq)
-from @NHISNSC_rawdata.@NHIS_60T a, @NHISNSC_rawdata.@NHIS_20T b
+from @NHISNSC_rawdata.@@NHIS_60T a, @NHISNSC_rawdata.@NHIS_20T b
 where a.key_seq=b.key_seq
 and a.div_cd in (select distinct source_code
 	from (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) as c)
 
 -- 참고) 60T 중 1:N 매핑 중복 건수
 select count(a.key_seq), sum(cnt)
-from @NHISNSC_rawdata.@NHIS_60T a, 
+from @NHISNSC_rawdata.@@NHIS_60T a, 
 	(select source_code, count(source_code)-1 as cnt 
 	from (select * from @NHISNSC_database.@source_to_concept_map where domain_id='procedure' and invalid_reason is null) as m
 	group by source_code 
@@ -71,7 +72,7 @@ from @NHISNSC_rawdata.@NHIS_60T a,
 where a.div_cd=b.source_code
 and a.key_seq=c.key_seq
 
-
+*/
 
 /**************************************
  2. 테이블 생성
@@ -130,7 +131,7 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 			case when x.dd_mqty_exec_freq is not null and isnumeric(x.dd_mqty_exec_freq)=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
 			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
-	FROM @NHISNSC_rawdata.NHIS_30T x, 
+	FROM @NHISNSC_rawdata.@NHIS_30T x, 
 		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='130') y
 	WHERE x.key_seq=y.key_seq
 	AND x.seq_no=y.seq_no) a, (select * from #mapping_table where domain_id='procedure' and invalid_reason is null and concept_invalid_reason is null) b
@@ -162,7 +163,7 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 			case when x.dd_exec_freq is not null and isnumeric(x.dd_exec_freq)=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
 			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
-	FROM @NHISNSC_rawdata.NHIS_60T x, 
+	FROM @NHISNSC_rawdata.@NHIS_60T x, 
 		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='160') y
 	WHERE x.key_seq=y.key_seq
 	AND x.seq_no=y.seq_no) a, (select * from #mapping_table where domain_id='procedure' and invalid_reason is null and concept_invalid_reason is null) b
@@ -179,7 +180,7 @@ INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE
 SELECT
 	convert(bigint, convert(varchar, a.master_seq) + convert(varchar, row_number() over (partition by a.key_seq, a.seq_no order by b.target_concept_id))) as procedure_occurrence_id,
 	a.person_id as person_id,
-	0 END as procedure_concept_id,
+	0 as procedure_concept_id,
 	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
 	45756900 as procedure_type_concept_id,
 	NULL as modifier_concept_id,
@@ -194,7 +195,7 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 			case when x.dd_mqty_exec_freq is not null and isnumeric(x.dd_mqty_exec_freq)=1 and cast(x.dd_mqty_exec_freq as float) > '0' then cast(x.dd_mqty_exec_freq as float) else 1 end as dd_mqty_exec_freq,
 			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
-	FROM @NHISNSC_rawdata.NHID_GY30_T1 x, 
+	FROM @NHISNSC_rawdata.@NHIS_30T x, 
 		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='130') y
 	WHERE x.key_seq=y.key_seq
 	AND x.seq_no=y.seq_no) a 
@@ -211,7 +212,7 @@ INSERT INTO @NHISNSC_database.PROCEDURE_OCCURRENCE
 SELECT 
 	convert(bigint, convert(varchar, a.master_seq) + convert(varchar, row_number() over (partition by a.key_seq, a.seq_no order by b.target_concept_id))) as procedure_occurrence_id,
 	a.person_id as person_id,
-	0 END as procedure_concept_id,
+	0 as procedure_concept_id,
 	CONVERT(VARCHAR, a.recu_fr_dt, 112) as procedure_date,
 	45756900 as procedure_type_concept_id,
 	NULL as modifier_concept_id,
@@ -226,7 +227,7 @@ FROM (SELECt x.key_seq, x.seq_no, x.recu_fr_dt, x.div_cd,
 			case when x.dd_exec_freq is not null and isnumeric(x.dd_exec_freq)=1 and cast(x.dd_exec_freq as float) > '0' then cast(x.dd_exec_freq as float) else 1 end as dd_exec_freq,
 			case when x.dd_mqty_freq is not null and isnumeric(x.dd_mqty_freq)=1 and cast(x.dd_mqty_freq as float) > '0' then cast(x.dd_mqty_freq as float) else 1 end as dd_mqty_freq,
 			y.master_seq, y.person_id
-	FROM @NHISNSC_rawdata.NHIS_60T x, 
+	FROM @NHISNSC_rawdata.@NHIS_60T x, 
 		 (select master_seq, key_seq, seq_no, person_id from @NHISNSC_database.SEQ_MASTER where source_table='160') y
 	WHERE x.key_seq=y.key_seq
 	AND x.seq_no=y.seq_no) a 
